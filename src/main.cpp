5d90831e4ea43454186c951b145a7d7828c1ecc8
tsukisuperior <tsukisuperior@gmail.com>
2020-10-08 07:10:17 -0500

    add some more files

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,21 @@ int main(int argc, char *argv[])
       "Nichijou Advanced",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
-      640,
-      480,
-      0);
+      240,
+      160,
+      SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALLOW_HIGHDPI
+
+  );
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(renderer);
   SDL_RenderPresent(renderer);
 
+  SDL_RenderDrawPoint(renderer, 100, 100); //Renders on middle of screen.
+  SDL_RenderPresent(renderer);
   SDL_Delay(3000);
 
   SDL_DestroyWindow(window);
@@ -24,3 +30,4 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+
