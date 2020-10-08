2f4a53b0a3288917fc44dc54962de2d2958e5050
tsukisuperior <tsukisuperior@gmail.com>
2020-10-08 01:33:26 -0500

    removed sdl2 source it wasnt needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
-#include "SDL.h" 
+#include "SDL.h"
 
 int main(int argc, char *argv[])
 {
   SDL_Init(SDL_INIT_VIDEO);
 
   SDL_Window *window = SDL_CreateWindow(
-    "Nichijou Advanced",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+      "Nichijou Advanced",
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      640,
+      480,
+      0);
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
